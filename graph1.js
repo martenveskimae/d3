0872c42d39db83f1c12363d90493f787ee1bd288
martenveskimae <marten.veskimae@gmail.com>
2016-06-07 09:12:42 +0300

    Add files via upload

---
+++ graph1.js
@@ -0,0 +1,338 @@
+function graph1(csvpath, color) {
+  if (color == "blue") {
+    colorrange = ["#045A8D", "#2B8CBE", "#74A9CF", "#A6BDDB", "#D0D1E6", "#F1EEF6"];
+  }
+  else if (color == "pink") {
+    colorrange = ["#980043", "#DD1C77", "#DF65B0", "#C994C7", "#D4B9DA", "#F1EEF6"];
+  }
+  else if (color == "party") {
+    colorrange = ["#109618", "#ff9900", "#3366cc", "#dc3912"];
+  }
+  else if (color == "orange") {
+    colorrange = ["#B30000", "#E34A33", "#FC8D59", "#FDBB84", "#FDD49E", "#FEF0D9"];
+  };
+
+  strokecolor = colorrange[0];
+  strokecolor2 = function (d, i) { return z(i);};
+
+  var format = d3.time.format("%m/%d/%y"),
+  bisectDate = d3.bisector(function(d) { return d.date; }).left;
+
+  var margin = {top: 50, right: 40, bottom: 60, left: 80},
+  width = 600 - margin.left - margin.right,
+  height = 300 - margin.top - margin.bottom;
+
+  var x = d3.time.scale()
+  .range([0, width]);
+
+  var y = d3.scale.linear()
+  .range([height, 0]);
+
+  var z = d3.scale.ordinal()
+  .range(colorrange);
+
+  var yAxis = d3.svg.axis()
+  .scale(y)
+  .orient("left")
+  .tickSize(-width, 0, 4);
+
+  var xAxis1 = d3.svg.axis()
+  .scale(x)
+  .tickFormat(EST.timeFormat("%B"))
+  .tickSize(5,0)
+  .orient("bottom")
+  .ticks(10)
+  .tickSize(-height, 0, 4);
+
+  var xAxis2 = d3.svg.axis()
+  .scale(x)
+  .ticks(d3.time.years, 1)
+  .tickFormat(d3.time.format("%Y"))
+  .tickSize(5,0)
+  .orient("bottom");
+
+  var line = d3.svg.line()
+  .interpolate("cardinal")  
+  .x(function(d) { return x(d.date); })
+  .y(function(d) { return y(d.value); });
+
+  var line2 = d3.svg.line()
+  .interpolate("cardinal")  
+  .x(function(d) { return x(d.date); })
+  .y(function(d) { return y(d.exp); });
+
+  var svg = d3.select(".bilanss").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var title = svg.append("text")
+  .attr("x", width/2)
+  .attr("y", -15)
+  .attr("class", "graphTitle")
+  .style("text-anchor", "middle")
+  .style("font-size", "14px");
+
+  var graph = d3.csv(csvpath, function(data) {
+    data.forEach(function(d) {
+      d.date = new Date(+d.year, +d.month-1, 1);
+      d.membership = +d.membership;
+      d.donation = +d.donation;
+      d.government = +d.government;
+      d.loan = +d.loan;
+      d.private = d.donation+d.membership;
+      d.sum = +d.total;
+      d.exp = +d.exp;
+      d.kvartal = +d.kvartal;
+      d.balance = +d.balance;
+      d.party = d.party;
+      d.value = d.kvartal;
+    });
+
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y.domain([d3.min(data, function(d) { return d.value; })-10, d3.max(data, function(d) { return d.value; })+10]);
+
+    Axis();
+
+    var nest = d3.nest()
+    .key(function(d) { return d.party; })
+    .entries(data.sort(function(a, b){ return a.date - b.date; }));
+    var transitionTime = 500;
+
+    nest.forEach(function(d, i) {
+      var partyLines = svg.append("g")
+      .datum(data);
+
+      partyLines.append("path")
+      .attr("class", "line")
+      .style("fill", "none");
+
+      partyLines.append("text")
+      .attr("class", "partyText")
+      .attr("dy", ".35em");
+    });
+
+    income()
+    function income() {
+      yAxis.tickValues([-80, -40, 0, 40, 80, 120]);
+      y.domain([d3.min(data, function(d) { return d.value; })-10, d3.max(data, function(d) { return d.value; })+10]);
+
+      d3.select(".y.axis")
+      .transition()
+      .duration(transitionTime)
+      .ease("sin-in-out")
+      .call(yAxis);
+      d3.select(".y.axis").selectAll(".tick text")
+      .transition()
+      .duration(transitionTime)
+      .attr("transform", "translate(-5, 0)");
+
+      d3.selectAll(".line")
+      .data(nest)
+      .transition()
+      .duration(200)
+      .attr("d", function(d){ return line(d.values); })
+      .style("stroke", function(d,i){ return z(i); });
+
+      d3.selectAll(".partyText")
+      .data(nest)
+      .transition()
+      .duration(200)
+      .attr("transform", function(d,i) {
+        var lastValue = nest[i].values[(nest[i].values.length-1)].value;
+        return "translate(" + (width+3) + "," + y(lastValue) + ")"; })
+      .text(function(d,i) { return data[i].party; });
+
+      d3.select(".graphTitle")
+      .transition()
+      .duration(200)
+      .text("Joonis 1. Erakondade sissetuleku muutus võrrelduna eelmise aasta sama kvartaliga");
+
+      d3.select(".zeroLine")
+      .transition()
+      .duration(transitionTime)
+      .attr("y1", y(0))
+      .attr("y2", y(0));
+    }
+
+    function exp(){
+      yAxis.tickValues([0, 20, 40, 60, 80, 100]);
+      y.domain([d3.min(data, function(d) { return d.exp; })-10, d3.max(data, function(d) { return d.exp; })+10]);
+
+      d3.select(".y.axis")
+      .transition()
+      .duration(transitionTime)
+      .ease("sin-in-out")
+      .call(yAxis);
+      d3.select(".y.axis").selectAll(".tick text")
+      .transition()
+      .duration(transitionTime)
+      .attr("transform", "translate(-5, 0)");
+
+      d3.selectAll(".line")
+      .data(nest)
+      .transition()
+      .duration(transitionTime)
+      .attr("d", function(d){ return line2(d.values); })
+      .style("stroke", function(d,i){ return z(i); });
+
+      d3.selectAll(".partyText")
+      .data(nest)
+      .transition()
+      .duration(transitionTime)
+      .attr("transform", function(d,i) {
+        var lastValue = nest[i].values[(nest[i].values.length-1)].exp;
+        return "translate(" + (width+3) + "," + y(lastValue) + ")"; })
+      .text(function(d,i) { return data[i].party; });
+
+      d3.select(".graphTitle")
+      .transition()
+      .duration(200)
+      .text("Muu joonis");
+
+      d3.select(".zeroLine")
+      .transition()
+      .duration(transitionTime)
+      .attr("y1", y(0))
+      .attr("y2", y(0));
+    }
+
+    var switcher = [true];
+    svg.on("click", function(d){
+      if (!switcher[0]) {
+        income();
+      } else {
+        exp(); 
+      }
+      switcher[0] =! switcher[0];
+    });
+
+    var lines = document.getElementsByClassName('line');
+    var mouseG = svg.append("g")
+    .attr("class", "mouse-over-effects");
+
+    mouseG.append("path")
+    .attr("class", "mouse-line")
+    .style("stroke", "black")
+    .style("stroke-width", "1px")
+    .style("opacity", "0");
+
+    var mousePerLine = mouseG.selectAll('.mouse-per-line')
+    .data(nest)
+    .enter()
+    .append("g")
+    .attr("class", "mouse-per-line");
+
+    mousePerLine.append("circle")
+    .attr("r", 6)
+    .style("stroke", function(d,i) { return z(i); })
+    .style("fill", "none")
+    .style("stroke-width", "1px")
+    .style("opacity", "0");
+
+    mousePerLine.append("text")
+    .attr("transform", "translate(10,3)");
+
+    mouseG.append('svg:rect')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('fill', 'none')
+    .attr('pointer-events', 'all')
+    .on('mouseout', function() {
+      d3.select(".mouse-line")
+      .style("opacity", "0");
+      d3.selectAll(".mouse-per-line circle")
+      .style("opacity", "0");
+      d3.selectAll(".mouse-per-line text")
+      .style("opacity", "0");
+    })
+    .on('mouseover', function() {
+      d3.select(".mouse-line")
+      .style("opacity", "1");
+      d3.selectAll(".mouse-per-line circle")
+      .style("opacity", "1");
+      d3.selectAll(".mouse-per-line text")
+      .style("opacity", "1");
+    })
+    .on('mousemove', function() {
+      var mouse = d3.mouse(this);
+      d3.selectAll(".mouse-line")
+      .attr("d", function() {
+        var d = "M" + mouse[0] + "," + height;
+        d += " " + mouse[0] + "," + 0;
+        return d;
+      });
+
+      d3.selectAll(".mouse-per-line")
+      .attr("transform", function(d, i) {
+        var xDate = x.invert(mouse[0]),
+        bisect = d3.bisector(function(d) { return d.date; }).right;
+        idx = bisect(nest[i].values[i].value, xDate);
+
+        var beginning = 0,
+        end = lines[i].getTotalLength(),
+        target = null;
+
+        while (true){
+          target = Math.floor((beginning + end) / 2);
+          pos = lines[i].getPointAtLength(target);
+          if ((target === end || target === beginning) && pos.x !== mouse[0]) {
+            break;
+          }
+          if (pos.x > mouse[0])      end = target;
+          else if (pos.x < mouse[0]) beginning = target;
+          else break;
+        }
+
+        d3.select(this).select('text')
+        .attr("class", "tooltip")
+        .text(y.invert(pos.y).toFixed(0));
+
+        return "translate(" + mouse[0] + "," + pos.y +")";
+      });
+    });
+
+
+    function Axis(){
+      svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis1);
+      svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + (height+25) + ")")
+      .call(xAxis2);
+
+      yAxis.tickValues([-80, -40, 0, 40, 80, 120]);
+      svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("x", -7)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Muutus protsentides");
+
+      svg.selectAll(".y.axis").selectAll(".tick text")
+      .attr("transform", "translate(-5, 0)");
+      svg.selectAll(".x.axis").selectAll(".tick text")
+      .attr("transform", "translate(0, 3)");
+
+      svg.append("line")
+      .attr("class", "zeroLine")
+      .attr("x1", 0)
+      .attr("x2", width)
+      .attr("y1", y(0))
+      .attr("y2", y(0))
+      .attr("stroke-width", 1)
+      .attr("stroke", "black")
+    };
+
+  });
+
+
+};
---
+++ graph2.js
@@ -0,0 +1,97 @@
+function graph2(csvpath, color) {
+  if (color == "blue") {
+    colorrange = ["#045A8D", "#2B8CBE", "#74A9CF", "#A6BDDB", "#D0D1E6", "#F1EEF6"];
+  }
+  else if (color == "pink") {
+    colorrange = ["#980043", "#DD1C77", "#DF65B0", "#C994C7", "#D4B9DA", "#F1EEF6"];
+  }
+  else if (color == "party") {
+    colorrange = ["#109618", "#ff9900", "#3366cc", "#dc3912"];
+  }
+  else if (color == "orange") {
+    colorrange = ["#B30000", "#E34A33", "#FC8D59", "#FDBB84", "#FDD49E", "#FEF0D9"];
+  };
+
+var w = 600,
+    h = 300;
+
+var nodes = d3.range(200).map(function() { return {radius: Math.random() * 12 + 4}; }),
+    color = d3.scale.category10();
+
+var force = d3.layout.force()
+    .gravity(0.05)
+    .charge(function(d, i) { return i ? 0 : -2000; })
+    .nodes(nodes)
+    .size([w, h]);
+
+var root = nodes[0];
+root.radius = 0;
+root.fixed = true;
+
+force.start();
+
+var svg2 = d3.select(".total").append("svg:svg")
+    .attr("width", w)
+    .attr("height", h);
+
+svg2.selectAll("circle")
+    .data(nodes.slice(1))
+  .enter().append("svg:circle")
+    .attr("r", function(d) { return d.radius - 2; })
+    .style("fill", function(d, i) { return color(i % 3); });
+
+force.on("tick", function(e) {
+  var q = d3.geom.quadtree(nodes),
+      i = 0,
+      n = nodes.length;
+
+  while (++i < n) {
+    q.visit(collide(nodes[i]));
+  }
+
+  svg2.selectAll("circle")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+});
+
+svg2.append('svg:rect')
+  .attr('width', w)
+  .attr('height', h)
+  .attr('fill', 'none')
+  .attr('pointer-events', 'all')
+  .on("mousemove", function() {
+  var p1 = d3.mouse(this);
+  root.px = p1[0];
+  root.py = p1[1];
+  force.resume();
+});
+
+function collide(node) {
+  var r = node.radius + 16,
+      nx1 = node.x - r,
+      nx2 = node.x + r,
+      ny1 = node.y - r,
+      ny2 = node.y + r;
+  return function(quad, x1, y1, x2, y2) {
+    if (quad.point && (quad.point !== node)) {
+      var x = node.x - quad.point.x,
+          y = node.y - quad.point.y,
+          l = Math.sqrt(x * x + y * y),
+          r = node.radius + quad.point.radius;
+      if (l < r) {
+        l = (l - r) / l * .5;
+        node.x -= x *= l;
+        node.y -= y *= l;
+        quad.point.x += x;
+        quad.point.y += y;
+      }
+    }
+    return x1 > nx2
+        || x2 < nx1
+        || y1 > ny2
+        || y2 < ny1;
+  };
+}
+
+
+};
