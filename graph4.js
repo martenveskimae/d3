84ccf9cf39a9e2d37577e085463177c8818f74a3
martenveskimae <marten.veskimae@gmail.com>
2016-07-21 14:02:29 +0300

    Add files via upload

--- graph4.js
+++ graph4.js
@@ -121,6 +121,7 @@ function graph4(csvpath, color, location, w, h) {
       nestData = d3.nest()
       .key(function(d){return d.party;})
       .entries(nodesData
+        .sort(function(a,b) { return new Date(a.Datum).getTime() - new Date(b.Datum).getTime() } )
         .sort(function(a, b){ return d3.ascending(a.party, b.party); })
         .filter(function(d) { return d.small == 1; }));
 
@@ -149,6 +150,7 @@ function graph4(csvpath, color, location, w, h) {
       nestData2 = d3.nest()
       .key(function(d){return d.party;})
       .entries(nodesData
+        .sort(function(a,b) { return new Date(a.Datum).getTime() - new Date(b.Datum).getTime() } )
         .sort(function(a, b){ return d3.ascending(a.party, b.party); })
         .filter(function(d) { return d.small == 0; }));
 
@@ -220,7 +222,7 @@ function graph4(csvpath, color, location, w, h) {
     function bigLines() {
 
       d3.selectAll(".line2Graph4")
-      .data(nestData2.sort(function(a, b){ return a.date - b.date; }))
+      .data(nestData2)
       .transition()
       .duration(transitionTime)
       .attr("d", function(d){ return line(d.values); })
@@ -249,7 +251,7 @@ function graph4(csvpath, color, location, w, h) {
       .style("stroke", function(d){ return z(d.values[0].party); });
 
       d3.selectAll(".partyTextGraph4")
-      .data(nestData.sort(function(a, b){ return a.date - b.date; }))
+      .data(nestData)
       .attr("x", width-10)
       .attr("y", function(d,i){ 
         var lastValue = nestData[i].values[(nestData[i].values.length-1)].total;
